e0994c85dee57f713784500c4917662a75763e00
sandeep_ahuja <sandeep.ahuja@daffodilsw.com>
2016-04-01 17:19:14 +0530

    Laid down initial structure for Add searchbar and Settings view controller.

---
+++ ContactUtility/Classes/Modules/Settings/User Interface/Presenter/SettingsPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsPresenter.swift
+//  ContactUtility
+//
+//  Created by Hitesh on 01/04/16.
+//  Copyright © 2016 Daffodil. All rights reserved.
+//
+
+import UIKit
+
+class SettingsPresenter: NSObject {
+
+}
---
+++ ContactUtility/Classes/Modules/Settings/User Interface/View/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  ContactUtility
+//
+//  Created by Hitesh on 01/04/16.
+//  Copyright © 2016 Daffodil. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: NSObject {
+
+}
